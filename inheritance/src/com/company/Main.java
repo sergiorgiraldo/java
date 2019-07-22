bffce650912d687f188d944a721ed4de9b519cf9
sergio giraldo <sergio@giraldo.com.br>
2019-07-21 22:45:00 -0300

    simple inheritance

---
+++ inheritance/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+class Calculation {
+    int z;
+
+    public void addition(int x, int y) {
+        z = x + y;
+        System.out.println("The sum of the given numbers:"+z);
+    }
+
+    public void Subtraction(int x, int y) {
+        z = x - y;
+        System.out.println("The difference between the given numbers:"+z);
+    }
+}
+
+class My_Calculation extends Calculation {
+    public void multiplication(int x, int y) {
+        z = x * y;
+        System.out.println("The product of the given numbers:"+z);
+    }
+}
+
+class My_Calculation_AddJust2 extends Calculation {
+    public void addition(int x) {
+        z = x + 2;
+        System.out.println("Given number plus 2 is:"+z);
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        int a = 20, b = 10;
+        My_Calculation demo = new My_Calculation();
+        demo.addition(a, b);
+        demo.Subtraction(a, b);
+        demo.multiplication(a, b);
+    }
+}
